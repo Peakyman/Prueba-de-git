bce13ef599774c944a5cd8b94eadad746c8c734c
Peakyman <tobaldiaz14@gmail.com>
2023-06-23 21:57:22 -0400

    Programa OpenMP finalizado con comentarios

--- nprimosomp.cpp
+++ nprimosomp.cpp
@@ -4,13 +4,14 @@
 using namespace std;
 
 int main() {
-	int cantPrimos=0;
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+	int cantPrimos=0; //contador de numeros primos
+	ios_base::sync_with_stdio(false); //desactiva el vaciado automático del búfer de salida
+	cin.tie(NULL); //desactiva la sincronización con los flujos de entrada/salida de C en C++
     
-    const int limite = 500000000;
-    vector<bool> esPrimo(limite + 1, true);
-    esPrimo[0] = esPrimo[1] = false;
+    const int limite = 500000000; //numero limite de numeros a evaluar
+    vector<bool> esPrimo(limite + 1, true); //vector numeros, true quiere decir que es primo
+    esPrimo[0] = esPrimo[1] = false; //inicializa 0 y 1 como no primos (false)
+    //Inicializa recorrido for para descartar numeros no primos (Criba de Eratostenes)
     for (int i = 2; i * i <= limite; i++) {
         if (esPrimo[i]) {
             for (int j = i * i; j <= limite; j += i) {
@@ -18,6 +19,7 @@ int main() {
             }
         }
     }
+    //Imprimir de forma paralelizada los numeros primos, contarlos e imprimir la cantidad
     #pragma omp parallel
     {
     	#pragma omp for
